2f9c43dc508bb2049fc3ee20ab8f0ea155187bf5
amr <amr>
2017-01-04 18:43:59 +0200

    add sound effects

--- Primitive_Drawing/CollisionDetector.cpp
+++ Primitive_Drawing/CollisionDetector.cpp
@@ -1,4 +1,5 @@
 #include "CollisionDetector.h"
+#include<windows.h>
 
 void CollisionDetector::colliderPosition( Shape ** shapes,int shapesCount, const vec3 & oldPosition, vec3 & newPosition)
 {
@@ -7,18 +8,21 @@ void CollisionDetector::colliderPosition( Shape ** shapes,int shapesCount, const
 		double radius = shapes[i]->getScale().x;
 		vec4 center = shapes[i]->getModelMatrix() * vec4(0.0f, 0.0f, 0.0f, 1.0f);
 		vec4 temporary = vec4(newPosition, center.w) - center;
+		
 		if (!shapes[i]->isSkyBox() && length(temporary) < radius + 0.5f)
 		{
-
+			mciSendString("play crash from 0", 0, 0, 0);
 			temporary = normalize(temporary);
 			temporary *= (radius + 0.75f);
 			temporary += center;
 			newPosition.x = temporary.x;
 			newPosition.y = temporary.y;
 			newPosition.z = temporary.z;
+			
 		}
 		else if (shapes[i]->isSkyBox() && length(temporary) > radius - 0.5f)
 		{
+			mciSendString("play crash from 0", 0, 0, 0);
 			temporary = normalize(temporary);
 			temporary *= (radius - 0.75f);
 			temporary += center;
--- Primitive_Drawing/main.cpp
+++ Primitive_Drawing/main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <gl/glew.h>
 #include <gl/glfw3.h>
+#include <thread>
 #include "ApplicationManager.h"
 
  ApplicationManager* applicationManger;
@@ -10,9 +11,23 @@ extern "C" {
 	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
+void playMainSound()
+{
+	mciSendString("open mainSound.mp3 alias mainSound", 0, 0, 0);
+	mciSendString("play mainSound repeat", 0, 0, 0);
+}
 int main( void )
 {
+	
+	//PlaySound("sound2.wav", NULL, SND_ASYNC | SND_LOOP|SND_FILENAME);
+
+	mciSendString("open crash.mp3 alias crash", 0, 0, 0);
+	std::thread helperThread(playMainSound);
+
 	InitGraphics();
+	mciSendString("close mainSound", 0, 0, 0);
+	mciSendString("close crash", 0, 0, 0);
+	helperThread.detach();
 	return 0;
 }
 
