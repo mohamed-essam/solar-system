91f2bd1d2a128daa8063d4b5d6c1bfd206888839
amr <amr>
2017-01-04 15:55:32 +0200

    work on nvidia or AMD card if available

--- Primitive_Drawing/main.cpp
+++ Primitive_Drawing/main.cpp
@@ -6,7 +6,10 @@
 
  ApplicationManager* applicationManger;
 bool InitGraphics();
-
+extern "C" {
+	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
 int main( void )
 {
 	InitGraphics();
